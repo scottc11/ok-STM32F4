b482ba8b1595c602a2631a0ae85926787e839263
scottc11 <scott.m.campbell11@gmail.com>
2024-04-06 10:07:06 -0400

    missed stashing this change

--- HAL/Inc/gpio_api.h
+++ HAL/Inc/gpio_api.h
@@ -19,7 +19,7 @@ void enable_adc_pin(PinName pin);
 void set_pin_pull(GPIO_InitTypeDef *config, PinMode mode);
 
 void gpio_set_pin_speed(PinName pin, GPIO_Speed speed);
-void gpio_config_input_capture(PinName pin, uint32_t alt_func_id);
+void gpio_config_input_capture(PinName pin);
 GPIO_PinState gpio_read_pin(PinName pin);
 void gpio_irq_set(PinName pin, IrqEvent event, bool enable);
 IRQn_Type gpio_get_irq_line(PinName pin);
