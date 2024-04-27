6839dc934927c95f6759ae1b20735b865a0eee40
scottc11 <scott.m.campbell11@gmail.com>
2024-04-27 09:33:49 -0400

    find closest float function

--- utils/Algorithms.h
+++ utils/Algorithms.h
@@ -4,7 +4,10 @@
 int invertIntInRange(int min, int max, int num);
 
 int getClosestInt(int val1, int val2, int target);
+
 float getClosestFloat(float val1, float val2, float target);
+
+float findClosestValue(float array[], int size, float target);
 
 template <class T>
 T map_num_in_range(T input, T minInput, T maxInput, T minOutput, T maxOutput)
