73157d6b0d91772aa5c583641342c090843bfe57
scottc11 <scott.m.campbell11@gmail.com>
2024-05-27 19:09:26 -0400

    listed hardware timer resolutions

--- HAL/Inc/tim_api.h
+++ HAL/Inc/tim_api.h
@@ -7,6 +7,18 @@
 class Metronome; // forward declaration
 class HardwareTimer; // forward declaration
 
+/*
+Advanced control:
+TIM1, TIM8 :: 16-bit
+
+General purpose:
+TIM2, TIM5 :: 32-bit
+TIM3, TIM4, TIM9, TIM10, TIM11 TIM12, TIM13, TIM14 :: 16-bit
+
+Basic:
+TIM6, TIM7 :: 16-bit
+*/
+
 uint32_t tim_get_APBx_freq(TIM_HandleTypeDef *htim);
 
 uint32_t tim_get_overflow_freq(TIM_HandleTypeDef *htim);
