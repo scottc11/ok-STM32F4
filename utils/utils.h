f574b95c8d5b3ac306980a836ee744f6e44fd59b
scottc11 <scott.m.campbell11@gmail.com>
2025-03-06 08:57:11 -0500

    array average template

--- utils/utils.h
+++ utils/utils.h
@@ -20,6 +20,17 @@ constexpr T ok_clamp(T x, T min, T max)
     return x < min ? min : (x > max ? max : x);
 }
 
+template <typename T>
+T ok_arr_average(T arr[], int n)
+{
+    // Find sum of array element
+    T sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+
+    return sum / n;
+}
+
 uint16_t ok_float_to_u16(float f, float min, float max);
 
 uint16_t ok_voltage_to_data(float targetVoltage, float minVoltage, float maxVoltage, uint16_t resolution);
