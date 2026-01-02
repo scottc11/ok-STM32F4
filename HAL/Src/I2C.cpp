a73417ecacfaddb063893bd1d7821e83d15a8e08
scottc11 <scott.m.campbell11@gmail.com>
2026-01-02 09:32:49 -0500

    i2c init now uses OK_ERROR_HANDLER

--- HAL/Src/I2C.cpp
+++ HAL/Src/I2C.cpp
@@ -64,7 +64,7 @@ void I2C::init()
     _hi2c.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
     status = HAL_I2C_Init(&_hi2c);
     if (status != HAL_OK) {
-        logger_log_err("I2C->init", status);
+        OK_ERROR_HANDLER(status, "HAL_I2C_Init");
     }
 
     if (_mode == NonBlocking) {
@@ -113,7 +113,7 @@ HAL_StatusTypeDef I2C::write(int address, uint8_t *data, int length, bool repeat
     status = HAL_I2C_Master_Transmit(&_hi2c, address, data, length, HAL_MAX_DELAY);
     if (status != HAL_OK)
     {
-        logger_log_err("I2C->write", status);
+        OK_ERROR_HANDLER(status, "HAL_I2C_Master_Transmit");
     }
     mutex.unlock();
     return status;
@@ -137,7 +137,7 @@ int I2C::read(int address, uint8_t *data, int length, bool repeated /*=false*/)
 
     status = HAL_I2C_Master_Receive(&_hi2c, address, data, length, HAL_MAX_DELAY);
     if (status != HAL_OK) {
-        logger_log_err("I2C->read", status);
+        OK_ERROR_HANDLER(status, "HAL_I2C_Master_Receive");
     }
     mutex.unlock();
     return status;
